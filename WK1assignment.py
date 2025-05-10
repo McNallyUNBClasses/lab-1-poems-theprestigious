5ba8a19741fe7260ecead8c3d67b18c8ab710c58
theprestigious <108156551+theprestigious@users.noreply.github.com>
2025-05-10 15:08:58 -0300

    Add files via upload

---
+++ WK1assignment.py
@@ -0,0 +1,20 @@
+print(3+4)
+
+print("hello Yoana")
+
+
+print(3*4)
+
+print(3/4)
+
+print(bin(10))
+
+print(hex(10))
+
+import datetime; print(datetime.datetime.now())
+
+import math; print(math.factorial(5))
+
+print([x for x in range(21) if x % 2 == 0])
+
+import math; print(math.sqrt(25))
